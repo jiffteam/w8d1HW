e6686ebf10e7d0974a23c2c54b573a2cc19e5793
kameron <kamscott3@gmail.com>
2022-05-20 16:13:24 -0400

    meep

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
 
 // middleware & static files
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consectur'},
-        {title: 'Mario finds stars', snippet: 'lorem ipsum dolor sit amet consectur'},
-        {title: 'How to defeat bowser', snippet: 'lorem ipsum dolor sit amet consectur'},
-    ];
-    res.render('index', {title: 'Home', blogs});
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
 
-
-
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
 
 app.get('/about', (req, res) => {
-    res.sendFile('about', { title: 'about'});
+  res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new Blog'});
-})
+// blog routes
+app.use('/blogs', blogRoutes);
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404'});
+  res.status(404).render('404', { title: '404' });
 });
-
