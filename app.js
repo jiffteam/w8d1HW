65564938036aaeaeb8af0195f6c44644a8995b1a
kameron <kamscott3@gmail.com>
2022-05-20 02:19:18 -0400

    boolin

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 const express = require('express');
-
+const morgan = require('morgan');
 // express app
 const app = express();
 
 // listen for requests
 app.listen(3000);
 
+// middleware & static files
+app.use(express.static('public'))
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname});
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consectur'},
+        {title: 'Mario finds stars', snippet: 'lorem ipsum dolor sit amet consectur'},
+        {title: 'How to defeat bowser', snippet: 'lorem ipsum dolor sit amet consectur'},
+    ];
+    res.render('index', {title: 'Home', blogs});
 });
+
+
+
+
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.sendFile('about', { title: 'about'});
 });
 
 // redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog'});
 })
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/about.html', { root: __dirname});
+    res.status(404).render('404', { title: '404'});
 });
 
